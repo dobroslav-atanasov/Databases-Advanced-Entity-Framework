5d789b2d29d1e1976f28b9052f360433a4f0f3ce
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-07-06 12:18:47 +0300

    Uploaded problems 1 and 2 from Code First - Exercises.

---
+++ 04.CodeFirst/Hospital.Data/EntityConfiguration/DiagnoseConfiguration.cs
@@ -0,0 +1,26 @@
+﻿namespace P01_HospitalDatabase.Data.EntityConfiguration
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using Models;
+
+    public class DiagnoseConfiguration : IEntityTypeConfiguration<Diagnose>
+    {
+        public void Configure(EntityTypeBuilder<Diagnose> builder)
+        {
+            builder.HasKey(d => d.DiagnoseId);
+
+            builder.Property(d => d.Name)
+                .IsUnicode()
+                .HasMaxLength(50);
+
+            builder.Property(d => d.Comments)
+                .IsUnicode()
+                .HasMaxLength(250);
+
+            builder.HasOne(d => d.Patient)
+                .WithMany(p => p.Diagnoses)
+                .HasForeignKey(d => d.PatientId);
+        }
+    }
+}
