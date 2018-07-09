9453b0a9ff10041b424b619ea5968851ca51aa25
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-07-09 13:24:35 +0300

    Uploaded problem Hospital of Code First - Exercises.

---
+++ 04.CodeFirst/Hospital/Hospital.Data/EntityConfiguration/VisitationConfiguration.cs
@@ -0,0 +1,29 @@
+﻿namespace P01_HospitalDatabase.Data.EntityConfiguration
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using Models;
+
+    public class VisitationConfiguration : IEntityTypeConfiguration<Visitation>
+    {
+        public void Configure(EntityTypeBuilder<Visitation> builder)
+        {
+            builder.HasKey(v => v.VisitationId);
+            
+            builder.Property(v => v.Comments)
+                .IsUnicode()
+                .HasMaxLength(250);
+
+            builder.HasOne(v => v.Patient)
+                .WithMany(p => p.Visitations)
+                .HasForeignKey(v => v.PatientId);
+
+            builder.Property(v => v.DoctorId)
+                .IsRequired(false);
+
+            builder.HasOne(v => v.Doctor)
+                .WithMany(d => d.Visitations)
+                .HasForeignKey(v => v.DoctorId);
+        }
+    }
+}
