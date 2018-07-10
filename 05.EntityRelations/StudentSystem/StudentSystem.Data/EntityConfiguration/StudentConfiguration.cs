a445e31e85da072769ec0c550cf437aaf4a3b69a
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-07-10 10:51:17 +0300

    Uploaded Student System of Entity Relations - Exercises.

---
+++ 05.EntityRelations/StudentSystem/StudentSystem.Data/EntityConfiguration/StudentConfiguration.cs
@@ -0,0 +1,30 @@
+﻿namespace P01_StudentSystem.Data.EntityConfiguration
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using Models;
+
+    public class StudentConfiguration : IEntityTypeConfiguration<Student>
+    {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder.HasKey(s => s.StudentId);
+
+            builder.Property(s => s.Name)
+                .IsRequired()
+                .IsUnicode()
+                .HasMaxLength(100);
+
+            builder.Property(s => s.PhoneNumber)
+                .IsRequired(false)
+                .IsUnicode(false)
+                .HasDefaultValueSql("CHAR(10)");
+
+            builder.Property(s => s.RegisteredOn)
+                .IsRequired();
+
+            builder.Property(s => s.Birthday)
+                .IsRequired(false);
+        }
+    }
+}
---
+++ 05.EntityRelations/StudentSystem/StudentSystem.Data/StudentSystemContext.cs
@@ -0,0 +1,49 @@
+﻿namespace P01_StudentSystem.Data
+{
+    using EntityConfiguration;
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext()
+        {
+        }
+
+        public StudentSystemContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Resource> Resources { get; set; }
+
+        public DbSet<Homework> HomeworkSubmissions { get; set; }
+
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if (!options.IsConfigured)
+            {
+                options.UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new StudentConfiguration());
+
+            builder.ApplyConfiguration(new CourseConfiguration());
+
+            builder.ApplyConfiguration(new ResourcesConfiguration());
+
+            builder.ApplyConfiguration(new HomeworkConfiguration());
+
+            builder.ApplyConfiguration(new StudentCourseConfiguration());
+        }
+    }
+}
