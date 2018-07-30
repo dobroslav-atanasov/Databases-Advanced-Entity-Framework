9a85a6cd0639738ea78b210e843b05a6c930ddf4
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-07-30 11:00:54 +0300

    Uploaded problem Product Shop.

---
+++ 10.XMLProcessing/ProductShop/ProductShop.Data/EntityConfiguration/CategoryProductConfiguration.cs
@@ -0,0 +1,22 @@
+﻿namespace ProductShop.Data.EntityConfiguration
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using Models;
+
+    internal class CategoryProductConfiguration : IEntityTypeConfiguration<CategoryProduct>
+    {
+        public void Configure(EntityTypeBuilder<CategoryProduct> builder)
+        {
+            builder.HasKey(cp => new { cp.CategoryId, cp.ProductId });
+
+            builder.HasOne(cp => cp.Category)
+                .WithMany(c => c.CategoryProducts)
+                .HasForeignKey(cp => cp.CategoryId);
+
+            builder.HasOne(cp => cp.Product)
+                .WithMany(p => p.CategoryProducts)
+                .HasForeignKey(cp => cp.ProductId);
+        }
+    }
+}
