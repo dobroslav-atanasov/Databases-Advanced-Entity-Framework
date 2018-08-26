92a3c9cd0f919ee27e3085f8b261f7be95a16873
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-08-26 12:07:01 +0300

    Uploaded exam - Fast Food.

---
+++ Exam-10.12.2018-FastFood/FastFood.Data/EntityConfiguration/PositionConfiguration.cs
@@ -0,0 +1,20 @@
+﻿namespace FastFood.Data.EntityConfiguration
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using Models;
+
+    public class PositionConfiguration : IEntityTypeConfiguration<Position>
+    {
+        public void Configure(EntityTypeBuilder<Position> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.HasAlternateKey(p => p.Name);
+
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(30);
+        }
+    }
+}
